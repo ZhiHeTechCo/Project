3dbc78ad731782e3339f0cc285c5457dd88b278b
wangfei-wangfei <wangfei-wangfei@163.com>
2016-11-24 18:02:15 +0800

    文件上传

---
+++ ItemBankWeb/src/zh/co/item/bank/web/file/controller/FileManageBean.java
@@ -0,0 +1,134 @@
+package zh.co.item.bank.web.file.controller;
+
+import java.io.File;
+
+import javax.inject.Named;
+import javax.servlet.http.Part;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+import org.springframework.context.annotation.Scope;
+
+import zh.co.common.constant.SystemConstants;
+import zh.co.common.controller.BaseController;
+import zh.co.common.exception.CmnSysException;
+import zh.co.common.exception.MessageId;
+import zh.co.common.log.CmnLogger;
+import zh.co.common.prop.PropertiesUtils;
+import zh.co.common.utils.FileUtils;
+
+/**
+ * <p>[概要] 文件管理Bean.</p>
+ * <p>[详细] </p>
+ * <p>[备考] </p>
+ * <p>[环境] JRE7.0</p>
+ * @author 王飞
+ * @since 1.0
+ */
+@Named("fileManageBean")
+@Scope("session")
+public class FileManageBean extends BaseController {
+
+	private final CmnLogger logger = CmnLogger.getLogger(this.getClass());
+    
+    
+    private UploadedFile uploadFile;
+    
+    private String fileName;
+    
+    private Part file;
+
+    
+    public Part getFile() {
+		return file;
+	}
+
+	public void setFile(Part file) {
+		this.file = file;
+	}
+
+	public String getPageId() {
+        return SystemConstants.PAGE_ITBK_USER_006;
+    }
+    
+    /**
+     * 初期化
+     * @return
+     */
+    public String init() {
+    	pushPathHistory("fileManageBean");
+
+        return SystemConstants.PAGE_ITBK_USER_006;
+    }
+
+    
+    /**
+     * 文件上传
+     * @param event
+     * @return
+     */
+    public String doUpload(FileUploadEvent event) {
+        try {
+
+          
+            uploadFile = event.getFile();
+            String fileName = FileUtils.getCurrentFileName(uploadFile.getFileName());
+            
+            String filePath = PropertiesUtils.getInstance().getSgValue(SystemConstants.FILEUPLOAD_PATH);
+            
+            //文件上传
+            FileUtils.uploadFile(uploadFile.getInputstream(), filePath, fileName);
+
+
+        } catch (Throwable e) {
+        	
+            processForException(logger, e);
+        }
+        return SystemConstants.PAGE_ITBK_USER_006;
+    }
+    
+    /**
+     * 文件下载
+     * @return
+     */
+    public void doDownLoad(String fileName) {
+        try {
+
+            // 文件路径
+            String filePath = PropertiesUtils.getInstance().getSgValue(SystemConstants.FILEUPLOAD_PATH);
+
+            String downloadPath = filePath + File.separator;
+            if (!FileUtils.checkExistFile(downloadPath + fileName)) {
+
+                throw new CmnSysException(MessageId.COMMON_E_0010, new Object[] { fileName });
+            }
+
+            try {
+                downloadFile(downloadPath, fileName);
+            } catch (Exception e) {
+                logger.log(MessageId.COMMON_E_0010, new Object[] { fileName }, e);
+                throw new CmnSysException(MessageId.COMMON_E_0010, new Object[] { fileName });
+            }
+        } catch (Throwable e) {
+            processForException(logger, e);
+        }
+    }
+
+	public UploadedFile getUploadFile() {
+		return uploadFile;
+	}
+
+	public void setUploadFile(UploadedFile uploadFile) {
+		this.uploadFile = uploadFile;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+
+}
