a3b23763ec8391fd5de7ef255f850355ba170590
wangfei-wangfei <wangfei-wangfei@163.com>
2016-12-15 15:33:55 +0800

    DB变更

---
+++ ItemBankWeb/src/zh/co/item/bank/db/entity/TbErrorReportBean.java
@@ -0,0 +1,63 @@
+package zh.co.item.bank.db.entity;
+
+public class TbErrorReportBean extends TbErrorReportBeanKey {
+    private Integer userId;
+
+    private Integer questionId;
+
+    private Integer mediaId;
+
+    private String result;
+
+    private String comment;
+
+    private String status;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Integer questionId) {
+        this.questionId = questionId;
+    }
+
+    public Integer getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(Integer mediaId) {
+        this.mediaId = mediaId;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result == null ? null : result.trim();
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment == null ? null : comment.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+}
---
+++ ItemBankWeb/src/zh/co/item/bank/db/entity/TbErrorReportBeanKey.java
@@ -0,0 +1,13 @@
+package zh.co.item.bank.db.entity;
+
+public class TbErrorReportBeanKey {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
