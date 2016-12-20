56552b1bb00c573616ea495e2b29fdfa3c2f653e
gyang <gyang@FAA11GL5W5.asiapacific.hpqcorp.net>
2016-12-20 22:54:46 +0800

    听力显示折行

--- ItemBankWeb/src/zh/co/item/bank/model/entity/MediaModel.java
+++ ItemBankWeb/src/zh/co/item/bank/model/entity/MediaModel.java
@@ -52,6 +52,9 @@ public class MediaModel extends TbMediaQuestionBean {
     /** radio样式 */
     private String radioClass;
 
+    /** 结果一览样式 */
+    private String divWidth;
+
     public int getUserId() {
         return userId;
     }
@@ -174,6 +177,14 @@ public class MediaModel extends TbMediaQuestionBean {
 
     public List<String> getContextList() {
         return CmnStringUtils.getSubjectList(this.getContext());
+    }
+
+    public String getDivWidth() {
+        return divWidth;
+    }
+
+    public void setDivWidth(String divWidth) {
+        this.divWidth = divWidth;
     }
 
 }
